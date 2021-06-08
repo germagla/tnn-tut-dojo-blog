3fec16cd0200931df43d09d71bc266def795d06a
ggt-kun <33932280+ggt-kun@users.noreply.github.com>
2021-06-08 11:47:52 +0000

    Add Delete button

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {useHistory, useParams} from "react-router-dom";
 import useFetch from "./useFetch";
 import BlogList from "./BlogList";
 import {Link} from "react-router-dom";
 
 function BlogDetails(props) {
     const {id} = useParams();
-    const {data: blog, error, isPending} = useFetch('https://mysterious-eyrie-58576.herokuapp.com/blogs/' + id)
+    const {data: blog, error, isPending} = useFetch('https://mysterious-eyrie-58576.herokuapp.com/blogs/' + id);
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('https://mysterious-eyrie-58576.herokuapp.com/blogs/'+blog.id, {
+            method: 'DELETE',
+        }).then(()=>{history.push('/tnn-tut-dojo-blog/')});
+    }
     return (
         <div className="blog-details">
             {error && <div><h1>{error}</h1></div>}
@@ -16,6 +23,7 @@ function BlogDetails(props) {
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author.toUpperCase()}</p>
                 <div>{blog.body}</div>
+                <button onClick={handleDelete}>Delete</button>
             </article>}
 
         </div>
